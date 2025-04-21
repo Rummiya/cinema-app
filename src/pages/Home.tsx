d33f8d4dcb71eb1a5d1cbc964718130083c874fd
Rummiya <rumibashirova@gmail.com>
2025-04-21 16:11:50 +0600

    refactor: очистила главную страницу, обновила описание коммитов

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,3 @@
-import { useEffect } from 'react';
-
 export const Home = () => {
-	useEffect(() => {
-		const options = {
-			method: 'GET',
-			headers: {
-				accept: 'application/json',
-				Authorization:
-					'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODMwMTgzNGY1YjRkOTY2ZDliMTVmYTA2NGFlZjVjNCIsIm5iZiI6MTc0NTE0MTI1Ny4xMzEsInN1YiI6IjY4MDRiZTA5ZTNmYWMyZjkwMjg5ZDAyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FzuzpurIRInumn1R70eZo-e1HbSP7NycF7kT982_BzI',
-			},
-		};
-
-		fetch('https://api.themoviedb.org/3/movie/1197306', options)
-			.then(res => res.json())
-			.then(res => console.log(res))
-			.catch(err => console.error(err));
-	}, []);
-
 	return <div>Home</div>;
 };
